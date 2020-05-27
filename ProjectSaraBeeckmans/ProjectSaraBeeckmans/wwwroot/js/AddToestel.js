3b752258aa89c5bacd21156d3dcf204ae106477b
SaraBeeckmans <sara.beeckmans@student.ap.be>
2020-05-27 19:41:31 +0200

    drop down

---
+++ ProjectSaraBeeckmans/ProjectSaraBeeckmans/wwwroot/js/AddToestel.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "/api/v1/supplier/list",
+        data: '$format=json',
+        dataType: 'json',
+        success: function (data) {
+
+            $.each(data, function (i, item) {
+                $("#inlineFormCustomSelect").append(
+                    "<option>" + item.name + "</option>"
+                )
+            });
+        }
+
+    })
+
+
+});
+
+function AddToestel(serienummer, aankoopdatum, prijs, garantie) {
+
+    var theUrl = "/api/v1/toestel/add?serieNummer=" + serienummer + "&aankoopDatum=" + aankoopdatum + "&prijs=" + prijs + "&garantie=" + garantie;
+    console.log(theUrl);
+    $.ajax({
+        type: "PUT",
+        url: theUrl,
+        data: '$format=json',
+        dataType: 'json',
+        success: function (data) {
+            console.log("toegevoegd");
+        }
+
+    })
+
+};
