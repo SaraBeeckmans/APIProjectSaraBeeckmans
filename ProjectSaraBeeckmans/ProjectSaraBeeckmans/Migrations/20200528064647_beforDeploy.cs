1ead49b72843c519797fcc3f1e9dac52d7687bd1
SaraBeeckmans <sara.beeckmans@student.ap.be>
2020-05-28 08:47:22 +0200

    Add migration

---
+++ ProjectSaraBeeckmans/ProjectSaraBeeckmans/Migrations/20200528064647_beforDeploy.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectSaraBeeckmans.Migrations
+{
+    public partial class beforDeploy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
